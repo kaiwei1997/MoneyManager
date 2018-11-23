b70a534397d5972bc6a3a88d10d8e0f557cfbdc9
Josh Tan <kaiweitan128@gmail.com>
2018-11-23 15:32:56 +0800

    CSC2074 Assg2

--- app/src/main/java/com/kaiwei/android/moneymanager/SplashScreen.java
+++ app/src/main/java/com/kaiwei/android/moneymanager/SplashScreen.java
@@ -1,24 +1,26 @@
 package com.kaiwei.android.moneymanager;
 
-import android.app.Activity;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager.LayoutParams;
-import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
-import android.view.animation.TranslateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
 public class SplashScreen extends AppCompatActivity {
 
     private ImageView splash;
-    private int SLEEP_TIMER = 2;
+    private TextView tv_availableBudget;
+    private TextView tv_totalSpent;
+    private TextView tv_balance;
+    private Button btn_start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,11 @@ public class SplashScreen extends AppCompatActivity {
 
         setContentView(R.layout.activity_splash_screen);
 
+        tv_availableBudget = (TextView)findViewById(R.id.tv_availableBudget);
+        tv_totalSpent = (TextView)findViewById(R.id.tv_totalSpent);
+        tv_balance =(TextView)findViewById(R.id.tv_balance);
+        btn_start =(Button)findViewById(R.id.btn_start);
+
         splash = (ImageView)findViewById(R.id.iv_Splash);
         Glide.with(this)
                 .load(R.drawable.splash_gif)
@@ -38,7 +45,22 @@ public class SplashScreen extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                splash.setImageDrawable(getResources().getDrawable(R.drawable.logo));
+                ObjectAnimator animatorY = ObjectAnimator.ofFloat(splash, "y", -1f);
+                animatorY.setDuration(1000);
+                animatorY.start();
+
+                tv_availableBudget.setVisibility(View.VISIBLE);
+                tv_totalSpent.setVisibility(View.VISIBLE);
+                tv_balance.setVisibility(View.VISIBLE);
+                btn_start.setVisibility(View.VISIBLE);
+                btn_start.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                        SplashScreen.this.finish();
+                    }
+                });
             }
         },3000);
 
