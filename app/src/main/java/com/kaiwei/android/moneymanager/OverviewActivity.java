cc1aa6f8c7ae26b2c51b58a1e4e901dddadd5ae3
Josh Tan <kaiweitan128@gmail.com>
2018-11-25 18:22:37 +0800

    csc2074

---
+++ app/src/main/java/com/kaiwei/android/moneymanager/OverviewActivity.java
@@ -0,0 +1,10 @@
+package com.kaiwei.android.moneymanager;
+
+import android.support.v4.app.Fragment;
+
+public class OverviewActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/kaiwei/android/moneymanager/OverviewFragment.java
@@ -0,0 +1,6 @@
+package com.kaiwei.android.moneymanager;
+
+import android.support.v4.app.Fragment;
+
+public class OverviewFragment extends Fragment {
+}
